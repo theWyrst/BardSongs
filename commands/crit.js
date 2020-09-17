47c3dc34a57b06b5b61568588ea3d7c01a402ba4
RyanK <43775990+theWyrst@users.noreply.github.com>
2020-09-17 18:30:38 -0500

    Finished crit.json

--- commands/crit.js
+++ commands/crit.js
@@ -5,14 +5,13 @@ module.exports = {
 
         const random = require('random');
 
-        const roll = random.int(min = 1, max = 14);
+        const roll = random.int(min = 1, max = 100);
 
-        const crittable = require('../crit.json');
-
-        console.log(crittable);
+        const crittable = require('./tables/crit.json');
 
         for(i = 0; i < crittable.crit.length; i++) {
             if(roll >= crittable.crit[i].min && roll <= crittable.crit[i].max) {
+                message.channel.send("You rolled a: " + roll);
                 message.channel.send(crittable.crit[i].effect);
             }
         }
